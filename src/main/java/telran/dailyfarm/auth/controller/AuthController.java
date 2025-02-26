8e844cf683524974d16132cb282d0823476ba700
Andrew <cheremisin2@gmail.com>
2025-02-27 00:38:10 +0200

    fix endpoints path

--- src/main/java/telran/dailyfarm/auth/controller/AuthController.java
+++ src/main/java/telran/dailyfarm/auth/controller/AuthController.java
@@ -11,6 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
+import telran.dailyfarm.auth.dto.FarmDto;
+import telran.dailyfarm.auth.dto.FarmRegisterDto;
+import telran.dailyfarm.auth.dto.UpdateFarmDto;
+import telran.dailyfarm.auth.dto.UpdateUserDto;
+import telran.dailyfarm.auth.dto.UserDto;
+import telran.dailyfarm.auth.dto.UserRegisterDto;
+import telran.dailyfarm.auth.service.AuthService;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,37 +25,42 @@ import lombok.RequiredArgsConstructor;
 public class AuthController {
   final AuthService authService;
 
-  @PostMapping("/user/register")
+  @PostMapping("/api/user/register")
   public UserDto registerUser(@RequestBody UserRegisterDto userRegisterDto) {
     return authService.registerUser(userRegisterDto);
   }
 
-  @PostMapping("/farm/register")
+  @PostMapping("/api/farm/register")
   public FarmDto registerFarm(@RequestBody FarmRegisterDto farmRegisterDto) {
     return authService.registerFarm(farmRegisterDto);
   }
 
-  @PostMapping("/user/login")
-  public UserDto login(Principal principal) {
+  @PostMapping("/api/user/login")
+  public UserDto loginUser(Principal principal) {
     return authService.getUser(principal.getName());
   }
 
-  @GetMapping("/user/{login}")
+  @PostMapping("/api/farm/login")
+  public FarmDto loginFarm(Principal principal) {
+    return authService.getFarm(principal.getName());
+  }
+
+  @GetMapping("/api/user/{login}")
   public UserDto getUser(@PathVariable String login) {
     return authService.getUser(login);
   }
 
-  @GetMapping("/farm/{id}")
+  @GetMapping("/api/farm/{id}")
   public FarmDto getFarm(@PathVariable String id) {
     return authService.getFarm(id);
   }
 
-  @PostMapping("/user/{login}")
+  @PostMapping("/api/user/{login}")
   public UserDto updateUser(@PathVariable String login, @RequestBody UpdateUserDto updateUserDto) {
     return authService.updateUser(login, updateUserDto);
   }
 
-  @PostMapping("/farm/{id}")
+  @PostMapping("/api/farm/{id}")
   public FarmDto updateFarm(@PathVariable String id, @RequestBody UpdateFarmDto updateFarmDto) {
     return authService.updateFarm(id, updateFarmDto);
   }
@@ -56,5 +68,13 @@ public class AuthController {
   @DeleteMapping("/user/{login}")
   public UserDto deleteAccount(@PathVariable String login) {
     return authService.deleteAccount(login);
+  }
+
+  @GetMapping("/user/logout")
+  public void logoutUser(Principal principal) {
+  }
+
+  @GetMapping("/farm/logout")
+  public void logoutFarm(Principal principal) {
   }
 }
